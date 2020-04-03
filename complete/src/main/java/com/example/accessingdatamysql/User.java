7c4f9341750a7252e40b5d1f1aacbb654d31b87c
Ai Beez <ndp69845@gmail.com>
2020-04-03 12:36:46 +0100

    First attempt at ehcache

--- complete/src/main/java/com/example/accessingdatamysql/User.java
+++ complete/src/main/java/com/example/accessingdatamysql/User.java
@@ -1,12 +1,23 @@
 package com.example.accessingdatamysql;
 
+import java.io.Serializable;
+
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
-@Entity // This tells Hibernate to make a table out of this class
-public class User {
+import org.hibernate.annotations.Cache;
+import org.hibernate.annotations.CacheConcurrencyStrategy;
+
+@Entity (name = "Userr")// This tells Hibernate to make a table out of this class
+@Cache(usage=CacheConcurrencyStrategy.READ_WRITE)
+public class User implements Serializable {
+	/**
+	 *
+	 */
+	private static final long serialVersionUID = 1L;
+
 	@Id
 	@GeneratedValue(strategy=GenerationType.AUTO)
 	private Integer id;
